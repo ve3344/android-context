8db77c1946fa7dc03ebcd56334a9d0b769200fa8
luowenbin <ve3344@qq.com>
2021-10-13 11:10:31 +0800

    build(gradle): add jitpack support

--- app/src/main/java/me/lwb/context/demo/MainActivity.kt
+++ app/src/main/java/me/lwb/context/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import me.lwb.context.AppContext
 
 class MainActivity : AppCompatActivity() {
